32ef6c922fb67cc87aaa6a4b329c8ada8bd1d8db
Italia Barron <italia.barron@cua.uam.mx>
2023-08-11 10:50:43 -0600

    Agregamos un comentario de prueba

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 /******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
+Esto es una prueba. Esto es un comentario sin sentido.
+Esto es una prueba. Esto es un comentario sin sentido.
 *******************************************************************************/
 #include <stdio.h>
 
